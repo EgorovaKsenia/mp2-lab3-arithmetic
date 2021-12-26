7d27f88b14d2f721a9c1ca2099cf01e50e45cc1f
Ksenya <ekkamilach@yandex.ru>
2021-12-26 13:02:07 +0300

    Update main_arithmetic.cpp
    .

--- samples/main_arithmetic.cpp
+++ samples/main_arithmetic.cpp
@@ -1,6 +1,20 @@
 // реализация пользовательского приложения
 
+#include "arithmetic.h"
+
 int main()
 {
-  return 0;
+	bool f = true;
+	while (f)
+	{
+		string s;
+		cout << "input string: ";
+		cin >> s;
+		Arithmetic A(s);
+		double res;
+		res = A.Calcul();
+		cout << res << endl;
+		cout << "continue?" << endl;
+		cin >> f;
+	}
 }
